b1c31148dfd371ac5e4f21ff42e5f03ba3f9fefc
Gijs Jespers <jespersgijs@gmail.com>
2024-02-08 16:04:25 +0000

    Load to Snowflake

--- project/cleanser/clean_wheather_data.py
+++ project/cleanser/clean_wheather_data.py
@@ -1,30 +1,86 @@
-from pyspark.sql import SparkSession, DataFrame
-from pyspark import SparkConf
+import json
+import logging
+from typing import Dict
 
-import pyspark.sql.functions as F
+import boto3
+import pyspark.sql.functions as sf
+from pyspark.sql import SparkSession
+import os
+from dotenv import load_dotenv
 
-def clean(frame: DataFrame) -> DataFrame:
-    clean_frame = (frame
-        .withColumn("lat", F.col("coordinates.latitude"))
-        .withColumn("lon", F.col("coordinates.longitude"))
-        .withColumn("date_local", F.col("date.local").cast("timestamp"))
-        .withColumn("date_utc", F.col("date.utc").cast("timestamp"))
-        .drop("date","coordinates")
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.StreamHandler())
+logger.setLevel(logging.INFO)
+
+load_dotenv()
+S3_DATA = os.environ["S3_DATA"]
+SECRET_ARN = os.environ["SECRET_ARN"]
+SNOWFLAKE_SCHEMA = os.environ["SNOWFLAKE_SCHEMA"]
+SNOWFLAKE_SOURCE_NAME = os.environ["SNOWFLAKE_SOURCE_NAME"]
+
+
+def get_snowflake_credentials():
+    sms = boto3.client("secretsmanager", region_name="eu-west-1")
+    secret = sms.get_secret_value(SecretId=SECRET_ARN)
+
+    return json.loads(secret["SecretString"])
+
+
+def get_spark_session(name: str = None) -> SparkSession:
+    return (
+        SparkSession.builder
+         .config(
+             "spark.jars.packages",
+             ",".join(
+                 [
+                     "org.apache.hadoop:hadoop-aws:3.3.4",
+                     "net.snowflake:spark-snowflake_2.12:2.12.0-spark_3.4",
+                     "net.snowflake:snowflake-jdbc:3.14.1",
+                 ]
+             ),
+        )
+        .config(
+            "fs.s3a.aws.credentials.provider",
+            "com.amazonaws.auth.DefaultAWSCredentialsProviderChain",
+        )
+        .appName(name)
+        .getOrCreate()
     )
-    return clean_frame
 
-if __name__ == "__main__":
-    config = {
-        "spark.jars.packages": "org.apache.hadoop:hadoop-aws:3.2.0",
-        "fs.s3a.aws.credentials.provider": "com.amazonaws.auth.DefaultAWSCredentialsProviderChain"
+
+def snowflake_config() -> Dict[str, str]:
+    credentials = get_snowflake_credentials()
+    return {
+        "sfURL": credentials["URL"],
+        "sfUser": credentials["USER_NAME"],
+        "sfPassword": credentials["PASSWORD"],
+        "sfDatabase": credentials["DATABASE"],
+        "sfWarehouse": credentials["WAREHOUSE"],
+        "sfRole": credentials["ROLE"],
+        "sfSchema": SNOWFLAKE_SCHEMA,
     }
 
-    conf = SparkConf().setAll(config.items())
-    spark = SparkSession.builder.config(conf=conf).getOrCreate()
 
-    # Extract
-    df = spark.read.json("s3a://dataminded-academy-capstone-resources/raw/open_aq/")
-    
-    clean_df = clean(df)
-    clean_df.show()
+if __name__ == "__main__":
+    spark = get_spark_session("ingest")
+    spark.sparkContext.setLogLevel("ERROR")
 
+    logger.info("Reading data from S3...")
+    df = spark.read.json(S3_DATA)
+
+    clean = df.select(
+        [
+            sf.col(colname)
+            if df.schema[colname].dataType.typeName() != "struct"
+            else sf.col(f"{colname}.*")
+            for colname in df.columns
+        ]
+    )
+
+    logger.info("Writing data to Snowflake...")
+
+    clean.write.format(SNOWFLAKE_SOURCE_NAME).options(**snowflake_config()).option(
+        "dbtable", "open_aq"
+    ).mode("overwrite").save()
+
+    logger.info("Done!")
